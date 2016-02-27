828312e2689e93556dff1e5ca6bee1f0380ab64d
Vyacheslav Voronenko <git@voronenko.info>
2016-02-27 23:51:10 +0200

    improve(recipe) demo recipe & basic documentation

---
+++ v1/bin/lookup_plugins/aws_secgroup_ids_from_names.py
@@ -0,0 +1,26 @@
+"""
+Description: This lookup takes an AWS region and a list of one or more
+security Group Names and returns a list of matching security Group IDs.
+Example Usage:
+{{ lookup('aws_secgroup_ids_from_names', ('eu-west-1', ['nginx_group', 'mysql_group'])) }}
+"""
+from ansible import errors
+try:
+    import boto.ec2
+except ImportError:
+    raise errors.AnsibleError("aws_secgroup_ids_from_names lookup cannot be run without boto installed")
+
+class LookupModule(object):
+    def __init__(self, basedir=None, **kwargs):
+        self.basedir = basedir
+
+    def run(self, terms, variables=None, **kwargs):
+        if isinstance(terms, basestring):
+            terms = [terms]
+        region = terms[0]
+        group_names = terms[1]
+        conn = boto.ec2.connect_to_region(region)
+        filters = {'group_name': group_names}
+        sg = conn.get_all_security_groups(filters=filters)
+        sg_list = [x.id.encode('utf-8') for x in sg]
+        return sg_list
---
+++ v1/bin/lookup_plugins/aws_subnet_ids_from_names.py
@@ -0,0 +1,27 @@
+"""
+Description: This lookup takes an AWS region and a list of one or more
+subnet names and returns a list of matching subnet ids.
+Example Usage:
+{{ lookup('aws_subnet_ids_from_names', ('eu-west-1', ['subnet1', 'subnet2'])) }}
+"""
+from ansible import errors
+try:
+    import boto.vpc
+except ImportError:
+    raise errors.AnsibleError("aws_subnet_ids_from_names lookup cannot be run without boto installed")
+
+class LookupModule(object):
+    def __init__(self, basedir=None, **kwargs):
+        self.basedir = basedir
+
+    def run(self, terms, inject=None, **kwargs):
+        if isinstance(terms, basestring):
+            terms = [terms]
+        subnet_ids = []
+        region = terms[0]
+        subnet_names = terms[1]
+        vpc_conn = boto.vpc.connect_to_region(region)
+        filters = {'tag:Name': terms[1]}
+        subnets = vpc_conn.get_all_subnets(filters=filters)
+        subnet_ids = [x.id.encode('utf-8') for x in subnets]
+        return subnet_ids
